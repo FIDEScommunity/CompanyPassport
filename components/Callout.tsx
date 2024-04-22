194dfbbb6a4b6013fadb9c7c9bf8e50e40363ace
Jan <github@janr.xyz>
2024-04-22 18:27:14 +0200

    fix: darkmode
    Signed-off-by: Jan <github@janr.xyz>

--- components/Callout.tsx
+++ components/Callout.tsx
@@ -3,7 +3,7 @@ import type { StyleVariant } from "./Container";
 
 // Custom CSS styles are defined in .globals.css to overwrite markdown
 const baseStyle =
-	"callout my-2 p-6 sm:p-8 sm:my-8 rounded-tr-2xl rounded-bl-2xl border-b-4 border-accent-2";
+	"callout my-4 p-6 sm:p-8 sm:my-8 rounded-tr-2xl rounded-bl-2xl border-b-4 border-accent-2";
 
 const calloutVariants = {
 	primary: "bg-primary-1 text-white",
